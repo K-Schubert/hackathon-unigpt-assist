47633d1e5660b164d373aa22cdcef006e7953f06
kieran.schubert <kieran.schubert@hesge.ch>
2023-11-24 19:44:35 +0100

    udpate rag

--- scripts/text_splitter.py
+++ scripts/text_splitter.py
@@ -1,7 +1,7 @@
 from langchain.text_splitter import CharacterTextSplitter
 
 text_splitter = CharacterTextSplitter(
-    separator = "Art\\.", # Utiliser une regex pour détecter "Art."
+    separator = "(Art\.|Article)", # Utiliser une regex pour détecter "Art.", "art.", et "Article" (insensible à la casse)
     chunk_size = 2000,
     chunk_overlap = 200,
     length_function = len,
