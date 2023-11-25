5ebace7021c76e6b6e834d5cb00fa674c25cca28
Malik Algelly <malik.algelly@unige.ch>
2023-11-25 14:01:55 +0100

    done

--- scripts/text_splitter.py
+++ scripts/text_splitter.py
@@ -1,7 +1,7 @@
 from langchain.text_splitter import CharacterTextSplitter
 
 text_splitter = CharacterTextSplitter(
-    separator = "Art\.? | Article\.?",
+    separator = "Art\.? | Article\.? | ARTICLE\. | ARTICLE | ART\.? | art\.",
     chunk_size = 200,
     chunk_overlap = 0,
     length_function = len,
